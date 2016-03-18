3a64025a6e85865d6c84d6188316da27bfbd12af
Kaveet Laxmidas <kaveetlaxmidas@gmail.com>
2016-03-18 01:24:38 -0500

    Make PebbleJS less chatty

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -3,20 +3,20 @@ Pebble.addEventListener('ready', function() {
 });
 
 Pebble.addEventListener('showConfiguration', function() {
-  var url = 'https://cdn.rawgit.com/kaveet/knibble/6c626c2d83dd6cc2488bd49f23bc629a7a383b02/config-page/config.html';
-  console.log('Showing configuration page: ' + url);
+  var url = 'https://cdn.rawgit.com/kaveet/knibble/5d2257c91f32d40179b43f12450747a860071e3e/config-page/config.html';
+  //console.log('Showing configuration page: ' + url);
 
   Pebble.openURL(url);
 });
 
 Pebble.addEventListener('webviewclosed', function(e) {
   var configData = JSON.parse(decodeURIComponent(e.response));
-  console.log('Configuration page returned: ' + JSON.stringify(configData));
+  //console.log('Configuration page returned: ' + JSON.stringify(configData));
 
-  var backgroundColor = configData['background_color'];
+  var highContrastMode = configData['high_contrast'];
 
   var dict = {};
-  if(configData['high_contrast'] === true) {
+  if(highContrastMode === 1) {
     dict[0] = 1;  // Send a boolean as an integer
   }
   else {
@@ -25,8 +25,8 @@ Pebble.addEventListener('webviewclosed', function(e) {
 
   // Send to watchapp
   Pebble.sendAppMessage(dict, function() {
-    console.log('Send successful: ' + JSON.stringify(dict));
+    //console.log('Send successful: ' + JSON.stringify(dict));
   }, function() {
-    console.log('Send failed!');
+    //console.log('Send failed!');
   });
 });
